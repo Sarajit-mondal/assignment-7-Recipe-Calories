2e62933621c4d9f4070eb494d6e13e74be6d29dc
Sarajit-mondal <bdsarajit499@gmail.com>
2024-03-15 20:16:04 +0600

    react icon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+
+// import { CiClock2 } from "react-icons/ci";
+// import { AiOutlineFire } from "react-icons/ai";
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <>
      <main>
       <h1 className='text-red-600'>asdkfs</h1>
+      
      </main>
     </>
   )
