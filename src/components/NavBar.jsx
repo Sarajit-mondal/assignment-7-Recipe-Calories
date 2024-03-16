149db844a3f796fbc49bbfa5884514fbf119ca22
Sarajit-mondal <bdsarajit499@gmail.com>
2024-03-16 07:50:55 +0600

    navbar complite

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,22 +3,22 @@ import { FaRegUserCircle } from "react-icons/fa";
 
 const NavBar = () => {
     return (
-        <nav>
+        <nav className="flex text-center gap-5 py-5  flex-col md:flex-row items-center justify-between">
             <h2>Recipe Calories</h2>
-            <div>
-                <ul>
+            <div className="flex flex-col md:flex-row items-center justify-between gap-5 md:gap-20">
+                <ul className="flex flex-col md:flex-row gap-4 md:gap-12">
                  <li><a href="#">Home</a></li>
                  <li><a href="#">Recipes</a></li>
                  <li><a href="#">About</a></li>
                  <li><a href="#">Search</a></li>
                 </ul>
 
-                <div>
-                    <div>
-                        <CiSearch></CiSearch>
-                        <input type="text" placeholder="Search"/>
+                <div className="flex items-center gap-3">
+                    <div className="bg-[#150B2B0D] flex items-center px-3 py-2 rounded-full">
+                        <CiSearch ></CiSearch>
+                        <input type="text" placeholder="Search" className="ml-1 bg-transparent placeholder:font-normal outline-none"/>
                     </div>
-                    <span><FaRegUserCircle></FaRegUserCircle></span>
+                    <span className="bg-green-500 p-3 rounded-full cursor-pointer"><FaRegUserCircle className="text-2xl text-[#150B2B99]"></FaRegUserCircle></span>
                 </div>
             </div>
         </nav>
